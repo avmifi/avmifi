f3bc82720c3b08b9289ba541e692d3ec6de71948
avmifi <avivfishman2000@gmail.com>
2024-10-18 23:46:32 +0300

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Shekels
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Session["userName"] = "Visitor";
+            Session["login"] = false;
+            Session["admin"] = false;
+            Session["number"] = "";
+            Session["email"] = "";
+            Session["gender"] = "Not Specified";
+            Session["country"] = "Not Specified";
+            Session["bday"] = "Not Specified";
+            Session["psw"] = "";
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Mpage.Master.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Shekels
+{
+    public partial class Mpage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!(bool)Session["login"])
+            { //unlogged
+                bar6.Style.Add("display", "none");
+                bar7.Style.Add("display", "none");
+                bar10.Style.Add("display", "none");
+            }
+            else
+            { //logged
+                bar2.Style.Add("display", "none"); //signup
+                bar3.Style.Add("display", "none"); //login
+                bar7.Style.Add("display", "block"); 
+                bar5.Style.Add("display", "block");
+                bar6.Style.Add("display", "none");
+                bar7.Style.Add("display", "block");
+                bar10.Style.Add("display", "block");
+
+            }
+
+            if (!(bool)Session["admin"])
+            { //unlogged
+                bar6.Style.Add("display", "none");
+            }
+            else
+            { //logged
+                
+                bar6.Style.Add("display", "block");
+                bar7.Style.Add("display", "block");
+            }
+
+
+        }
+    }
+}
